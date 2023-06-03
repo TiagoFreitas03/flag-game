aea54dd781de3c53de39c66a4926d9968fe7be05
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-06-03 19:13:43 -0300

    função para embaralhar um array

---
+++ src/utils/shuffle-array.ts
@@ -0,0 +1,12 @@
+export function shuffleArray(array: Array<any>) {
+  let currentIndex = array.length, randomIndex = 0
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array
+}
